9dbfc07e957c10824d3f48f5ea9ad7f82a319bb2
Mikey Shean <mdshean2@gmail.com>
2015-10-23 14:36:58 -0400

    Add IndexDetail tests.

--- polls/tests.py
+++ polls/tests.py
@@ -66,6 +66,28 @@ class QuestionViewTests(TestCase):
             ['<Question: Past1>', '<Question: Past2>']
         )
 
+class QuestiondIndexDetailTests(TestCase):
+
+    def test_detail_view_with_a_future_question(self):
+        """
+        Return 404 for a detail view of a question with a future
+        pub_date
+        """
+        future_question = create_question(question_text="Future", days=5)
+        response = self.client.get(reverse('polls:detail',
+                                    args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        Should return detail view of question with past pub_date
+        """
+        past_question = create_question(question_text="Past", days=-5)
+        response = self.client.get(reverse('polls:detail',
+                                            args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text, status_code=200)
+
+
 class QuestionMethodTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
--- polls/views.py
+++ polls/views.py
@@ -18,6 +18,10 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        # exclude questions that are not published yet
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
